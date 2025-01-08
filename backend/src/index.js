1e1e1002e63b0b5be74e47d49e1ca83fd09f462e
Rakesh Sharma <62244892+Xythercoder@users.noreply.github.com>
2025-01-08 10:53:52 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import path from "path";
+
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -15,6 +18,18 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use( cookieParser() );
+const cors = require( 'cors' );
+
+app.use(
+  cors({
+    origin: "https://chitchat.apstor.org:8090",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 
 
 app.use("/api/auth", authRoutes);
