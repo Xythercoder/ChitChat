ba6cb8dde291430f792731cd5334c7c6a1a8e060
Rakesh Sharma <62244892+Xythercoder@users.noreply.github.com>
2025-01-08 13:56:06 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,12 +20,29 @@ app.use(express.json());
 app.use( cookieParser() );
 const cors = require('cors');
 
+const allowedOrigins = [
+  "https://chitchat.apstor.org",  
+  "http://localhost:5173",       
+  "http://localhost:8090"        
+];
+
 app.use(
   cors({
-    origin: "https://chitchat.apstor.org",
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy: This origin is not allowed."));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"],   // Allowed headers
     credentials: true,
   })
 );
+
+
+app.options("*", cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chitchat.apstor.org"],
+    origin: ["http://localhost:5173", "https://chitchat.apstor.org","http://localhost:8090" ],
   },
 });
 
