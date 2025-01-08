e5ed0c29347a225fc93d01bd63e4e0495aa1625a
Rakesh Sharma <62244892+Xythercoder@users.noreply.github.com>
2025-01-08 07:42:34 +0530

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // You can use `window.location.hostname` to get the current domain/host
-const hostIP = window.location.hostname === "localhost" ? "http://localhost:5001" : "https://api.apstor.org/:5001";
+const hostIP = window.location.hostname === "localhost" ? "http://localhost:5001" : "https://api.apstor.org";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? `${hostIP}/api` : "/api",
