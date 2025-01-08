17cdcdf12ac54553732f5192e495b6331b3f71b2
Rakesh Sharma <62244892+Xythercoder@users.noreply.github.com>
2025-01-08 14:02:55 +0530

    s

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use( cookieParser() );
-const cors = require('cors');
+
 
 const allowedOrigins = [
   "https://chitchat.apstor.org",  
