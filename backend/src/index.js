62152923572741ec9c6aa751fd8b3b48b1835b84
Rakesh Sharma <62244892+Xythercoder@users.noreply.github.com>
2025-01-08 09:42:07 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,30 +2,39 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import { Server } from "socket.io";
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import { app } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+
 app.use(express.json());
 app.use( cookieParser() );
-const cors = require('cors');
+const cors = require( 'cors' );
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://chitchat.apstor.org",
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://chitchat.apstor.org");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true"); // If cookies are used
+  next();
+});
 
 
 app.use("/api/auth", authRoutes);
