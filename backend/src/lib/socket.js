f797419b33ebbaa840e75bd668c7f9f794df1e0d
Rakesh Sharma <62244892+Xythercoder@users.noreply.github.com>
2025-01-08 11:31:02 +0530

    done

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://chitchat.apstor.org:8090"],
+    origin: ["https://chitchat.apstor.org:8090"],
   },
 });
 
